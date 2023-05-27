5e5bf216050bc8d19f900f8c2c9faeb6b9c183c2
Fabio Mignone <mignofax@gmail.com>
2023-05-27 23:30:11 +0200

    Object Programming Paradigm(OPP)
    Modified the code to follow the paradigm of object programming (OPP) by moving some methods within specific classes

---
+++ Project_Folder/PingApp/Logic/StatisticsHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingApp.Logic
+{
+    public class StatisticsHelper
+    {
+        public string CalculateTotalData(List<bool> data)
+        {
+            int dataOk = data.Count(d => d);
+            int dataLoss = data.Count(d => !d);
+            int percentage = (int)((dataOk / (double)data.Count) * 100);
+            return $"{dataOk} packets received, {dataLoss} packets lost, {percentage}% packet success";
+        }
+
+        public string CalculateTotalRoundTripTime(List<int> rtt)
+        {
+            if (rtt.Any())
+            {
+                int minRtt = rtt.Min();
+                int maxRtt = rtt.Max();
+                int avgRtt = (int)rtt.Average();
+                return $"Min RTT: {minRtt}ms, Max RTT: {maxRtt}ms, Avg RTT: {avgRtt}ms";
+            }
+            else
+            {
+                return "No packets received.";
+            }
+        }
+    }
+}
---
+++ Project_Folder/PingApp/Ping/PingManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+public class PingManager
+{
+    private List<int> RTT { get; set; }
+    private List<int> TTL { get; set; }
+    private List<bool> DATA { get; set; }
+
+    public PingManager()
+    {
+        RTT = new List<int>();
+        TTL = new List<int>();
+        DATA = new List<bool>();
+    }
+
+    public async Task<PingResult> PerformPingsAsync(string address)
+    {
+        try
+        {
+            IPAddress[] addresses = await Dns.GetHostAddressesAsync(address);
+            string realPing = addresses[0].ToString();
+            using (Ping pingSender = new Ping())
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    PingReply reply = await pingSender.SendPingAsync(realPing);
+
+                    if (reply != null && reply.Status == IPStatus.Success)
+                    {
+                        DATA.Add(true);
+                        RTT.Add((int)reply.RoundtripTime);
+                        TTL.Add(reply.Options?.Ttl ?? 0);
+                    }
+                    else
+                    {
+                        DATA.Add(false);
+                        RTT.Add(0);
+                        TTL.Add(0);
+                    }
+                }
+            }
+
+            return new PingResult(DATA, RTT, TTL);
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+}
+
+public class PingResult
+{
+    public List<bool> Data { get; private set; }
+    public List<int> Rtt { get; private set; }
+    public List<int> Ttl { get; private set; }
+
+    public PingResult(List<bool> data, List<int> rtt, List<int> ttl)
+    {
+        Data = data;
+        Rtt = rtt;
+        Ttl = ttl;
+    }
+}
